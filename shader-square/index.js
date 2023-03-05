54d61fda0fc430b174cecea5267c392e59f20cd5
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-03-05 12:35:01 +0900

    シェーダーで四角形を描く

---
+++ shader-square/index.js
@@ -0,0 +1,96 @@
+// @see https://thebookofshaders.com/02/?lan=jp
+
+import { utils } from '../common/js/utils.js'
+
+/** @type {WebGL2RenderingContext | null} */
+let gl = null
+/** @type {WebGLVertexArrayObject | null}*/
+let vao = null
+/** @type {WebGLProgram | null} */
+let program = null
+
+/**
+ * 適切な頂点シェーダーとフラグメントシェーダーでプログラムを作成する関数
+ */
+const initProgram = async () => {
+  const vertexShader = await utils.loadShader(gl, './index.vert')
+  const fragmentShader = await utils.loadShader(gl, './index.frag')
+  program = utils.getProgram(gl, vertexShader, fragmentShader)
+  program.aVertexPosition = gl.getAttribLocation(program, 'a_position')
+  gl.useProgram(program)
+}
+
+/**
+ * バッファを準備する関数
+ */
+const initBuffers = () => {
+  /** 頂点 */
+  const vertices = [
+    -0.5, -0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, 0.5,
+  ]
+
+  const vertexBuffer = gl.createBuffer()
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW)
+
+  // VAOインスタンス作成
+  vao = gl.createVertexArray()
+  gl.bindVertexArray(vao)
+
+  const size = 2
+  const type = gl.FLOAT
+  const normalize = false
+  const stride = 0
+  const offset = 0
+  gl.vertexAttribPointer(
+    program.aVertexPosition,
+    size,
+    type,
+    normalize,
+    stride,
+    offset
+  )
+  gl.enableVertexAttribArray(program.aVertexPosition)
+
+  // バッファを使い終えたらバインドを解除
+  gl.bindVertexArray(null)
+  gl.bindBuffer(gl.ARRAY_BUFFER, null)
+}
+
+/**
+ * canvasに描画する関数
+ */
+const draw = () => {
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+
+  gl.bindVertexArray(vao)
+
+  const primitiveType = gl.TRIANGLES
+  const offset = 0
+  const count = 6 // indices.length
+  gl.drawArrays(primitiveType, offset, count)
+
+  // 利用が終わったバッファはバインドを解除
+  gl.bindVertexArray(null)
+  gl.bindBuffer(gl.ARRAY_BUFFER, null)
+}
+
+/**
+ * アプリケーションの初期化関数
+ */
+const init = async () => {
+  const canvas = utils.getCanvas('webgl-canvas')
+
+  utils.autoResizeCanvas(canvas)
+
+  gl = utils.getGLContext(canvas)
+  gl.clearColor(0, 0, 0, 0)
+
+  // 適切な順序で関数を呼び出す
+  await initProgram()
+  initBuffers()
+  draw()
+}
+
+window.onload = init
