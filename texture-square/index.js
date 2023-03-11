eaf4757dc7bffc8964f782e7dfe20929faa59a51
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-03-11 10:24:44 +0900

    テクスチャの表示に成功

--- texture-square/index.js
+++ texture-square/index.js
@@ -10,6 +10,8 @@ let canvas = null
 let gl = null
 /** @type {WebGLProgram | null} */
 let program = null
+/** @type {WebGLTexture | null} */
+let texture = null
 
 /** @type {number[]} */
 let index = []
@@ -30,10 +32,10 @@ const initProgram = async () => {
 
   program.aVertexPosition = gl.getAttribLocation(program, 'a_position')
   program.aVertexColor = gl.getAttribLocation(program, 'a_color')
-  //program.aTextureCoord = gl.getAttribLocation(program, 'a_textureCoord')
+  program.aTextureCoord = gl.getAttribLocation(program, 'a_textureCoord')
 
   program.uMvpMatrix = gl.getUniformLocation(program, 'u_mvpMatrix')
-  //program.uTexture = gl.getUniformLocation(program, 'u_texture')
+  program.uTexture = gl.getUniformLocation(program, 'u_texture')
 
   gl.useProgram(program)
 }
@@ -64,12 +66,12 @@ const initBuffers = async () => {
 
   // テクスチャ座標
   // prettier-ignore
-  //const textureCoord = [
-  //  0.0, 0.0,
-  //  1.0, 0.0,
-  //  0.0, 1.0,
-  //  1.0, 1.0
-  //]
+  const textureCoord = [
+    0.0, 0.0,
+    1.0, 0.0,
+    0.0, 1.0,
+    1.0, 1.0
+  ]
 
   // 頂点インデックス
   // prettier-ignore
@@ -93,11 +95,11 @@ const initBuffers = async () => {
   })
 
   // テクスチャ座標VBO
-  //utils.setAttribute(gl, {
-  //  vbo: utils.getVBO(gl, textureCoord),
-  //  location: program.aTextureCoord,
-  //  stride: 2, // vec2型（xy座標）
-  //})
+  utils.setAttribute(gl, {
+    vbo: utils.getVBO(gl, textureCoord),
+    location: program.aTextureCoord,
+    stride: 2, // vec2型（xy座標）
+  })
 
   // IBO
   const ibo = utils.getIBO(gl, index)
@@ -119,12 +121,11 @@ const initBuffers = async () => {
   // 共通の変換行列を作っておく
   pvMatrix = pMatrix.mulByMatrix4x4(vMatrix)
 
-  //const { texture } = utils.getTexture(gl, './img/tomixy-64x64.jpg')
-  //// 有効にするテクスチャユニットを指定
-  //// 使うテクスチャが一つなので、そのまま0番目のユニットを使う
-  //gl.activeTexture(gl.TEXTURE0)
-  //gl.bindTexture(gl.TEXTURE_2D, texture)
-  //gl.uniform1i(program.uTexture, 0)
+  // テクスチャを生成
+  texture = await utils.getTexture(gl, './img/tomixy-64x64.jpg')
+  // 有効にするテクスチャユニットを指定
+  // 使うテクスチャが一つなので、そのまま0番目のユニットを使う
+  gl.activeTexture(gl.TEXTURE0)
 }
 
 /**
@@ -133,6 +134,10 @@ const initBuffers = async () => {
 const draw = () => {
   // canvasを初期化
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+  // テクスチャをバインドしてuniform変数に登録
+  gl.bindTexture(gl.TEXTURE_2D, texture)
+  gl.uniform1i(program.uTexture, 0)
 
   // カウンタをインクリメント
   count++
