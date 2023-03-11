ac3e5927a898d8d2918a0d6f14cf69851db08bcc
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-03-11 10:24:59 +0900

    お試しのテクスチャ生成関数を削除

--- common/js/utils.js
+++ common/js/utils.js
@@ -229,46 +229,6 @@ const getTexture = (gl, source) => {
   })
 }
 
-const getLoadImageTexture = (gl, url, render) => {
-  const tex = gl.createTexture()
-  gl.bindTexture(gl.TEXTURE_2D, tex)
-  // Fill the texture with a 1x1 blue pixel.
-  gl.texImage2D(
-    gl.TEXTURE_2D,
-    0,
-    gl.RGBA,
-    1,
-    1,
-    0,
-    gl.RGBA,
-    gl.UNSIGNED_BYTE,
-    new Uint8Array([0, 0, 255, 255])
-  )
-
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
-
-  const textureInfo = {
-    width: 1, // we don't know the size until it loads
-    height: 1,
-    texture: tex,
-  }
-  const img = new Image()
-  img.addEventListener('load', function () {
-    textureInfo.width = img.width
-    textureInfo.height = img.height
-
-    gl.bindTexture(gl.TEXTURE_2D, textureInfo.texture)
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img)
-    gl.generateMipmap(gl.TEXTURE_2D)
-
-    render()
-  })
-  img.src = url
-
-  return textureInfo
-}
-
 /**
  * 頂点属性のバインドと登録を行う関数
  *
@@ -362,7 +322,6 @@ export const utils = {
   getVBO,
   getIBO,
   getTexture,
-  getLoadImageTexture,
   setAttribute,
   configureControls,
 }
