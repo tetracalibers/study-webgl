679ca3f44ce17b831b1607cf7b46926dcc8659c7
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-03-12 17:44:08 +0900

    3枚のテクスチャ四角形でステンシルテストを試す

--- stencil-buffer-squares/index.js
+++ stencil-buffer-squares/index.js
@@ -1,9 +1,9 @@
-// @see https://wgld.org/d/webgl/w021.html
+// @see https://wgld.org/d/webgl/w038.html
 
 import { utils } from '../common/js/utils.js'
 import { Matrix4x4 } from '../common/js/dist/matrix.js'
 import { Float32Vector3 } from '../common/js/dist/vector.js'
-import { torus } from './torus.js'
+import { Quaternion } from '../common/js/dist/quaternion.js'
 
 /** @type {HTMLCanvasElement | null} */
 let canvas = null
@@ -11,14 +11,44 @@ let canvas = null
 let gl = null
 /** @type {WebGLProgram | null} */
 let program = null
+/** @type {WebGLTexture | null} */
+let texture = null
 
 /** @type {number[]} */
 let index = []
 
 /** @type {Matrix4x4} */
+let pMatrix
+/** @type {Matrix4x4} */
 let pvMatrix
+let rotationByMouse = Matrix4x4.identity()
+
+// ライトベクトル
+const lightDirection = [1.0, 1.0, 1.0]
 
 let count = 0
+
+/**
+ * イベントから取得したマウス座標をもとに回転軸ベクトルと回転角を割り出し、
+ * クォータニオンを更新する関数
+ *
+ * @param {MouseEvent} e
+ */
+const calcQuaternionFromMousePosition = (e) => {
+  const cvsW = canvas.width
+  const cvsH = canvas.height
+  const cvsNorm = Math.sqrt(cvsW * cvsW + cvsH * cvsH)
+  // マウス座標をキャンバス内の座標に変換した上で
+  let x = e.clientX - canvas.offsetLeft
+  let y = e.clientY - canvas.offsetTop
+  // キャンバス中央からの相対的な座標に変換
+  x -= cvsW * 0.5
+  y -= cvsH * 0.5
+  const mouseNorm = Math.sqrt(x * x + y * y)
+  const angle = (mouseNorm * 2.0 * Math.PI) / cvsNorm
+  const axis = new Float32Vector3(y, x, 0.0).normalize()
+  rotationByMouse = Quaternion.rotationAround(axis, angle).toRotationMatrix4()
+}
 
 /**
  * 適切な頂点シェーダーとフラグメントシェーダーでプログラムを作成する関数
@@ -32,10 +62,12 @@ const initProgram = async () => {
   program.aVertexPosition = gl.getAttribLocation(program, 'a_position')
   program.aNormal = gl.getAttribLocation(program, 'a_normal')
   program.aVertexColor = gl.getAttribLocation(program, 'a_color')
+  program.aTextureCoord = gl.getAttribLocation(program, 'a_textureCoord')
 
   program.uMvpMatrix = gl.getUniformLocation(program, 'u_mvpMatrix')
   program.uMInvMatrix = gl.getUniformLocation(program, 'u_mInvMatrix')
   program.uLightDirection = gl.getUniformLocation(program, 'u_lightDirection')
+  program.uTexture = gl.getUniformLocation(program, 'u_texture')
 
   gl.useProgram(program)
 }
@@ -43,71 +75,67 @@ const initProgram = async () => {
 /**
  * バッファを準備する関数
  */
-const initBuffers = () => {
-  const torusData = torus(32, 32, 1.0, 2.0)
-  const { positions, colors, normals } = torusData
-  index = torusData.index
+const initBuffers = async () => {
+  const position = [-1.0, 1.0, 0.0, 1.0, 1.0, 0.0, -1.0, -1.0, 0.0, 1.0, -1.0, 0.0]
+  const normal = [0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0]
+  const color = [1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+  const textureCoord = [0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0]
+  index = [0, 1, 2, 3, 2, 1]
 
   // 頂点位置情報VBO
   utils.setAttribute(gl, {
-    vbo: utils.getVBO(gl, positions),
+    vbo: utils.getVBO(gl, position),
     location: program.aVertexPosition,
     stride: 3 // vec3型
   })
 
   // 法線情報VBO
   utils.setAttribute(gl, {
-    vbo: utils.getVBO(gl, normals),
+    vbo: utils.getVBO(gl, normal),
     location: program.aNormal,
     stride: 3
   })
 
   // 頂点色情報VBO
   utils.setAttribute(gl, {
-    vbo: utils.getVBO(gl, colors),
+    vbo: utils.getVBO(gl, color),
     location: program.aVertexColor,
     stride: 4 // vec4型
+  })
+
+  // テクスチャ座標VBO
+  utils.setAttribute(gl, {
+    vbo: utils.getVBO(gl, textureCoord),
+    location: program.aTextureCoord,
+    stride: 2 // vec2型（xy座標）
   })
 
   // IBO
   const ibo = utils.getIBO(gl, index)
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo)
 
-  // ビュー座標変換行列
-  const vMatrix = Matrix4x4.lookAt(
-    new Float32Vector3(0.0, 0.0, 20.0), // 三次元空間を映し出すカメラを後ろに 20.0 移動した状態で置く
-    new Float32Vector3(0.0, 0.0, 0.0), // 原点を注視点として見つめる
-    new Float32Vector3(0.0, 1.0, 0.0) // カメラの上方向は Y 軸の方向に指定
-  )
   // プロジェクション座標変換行列
-  const pMatrix = Matrix4x4.perspective({
+  pMatrix = Matrix4x4.perspective({
     fovYRadian: 45, // 視野角を 45 度
     aspectRatio: canvas.width / canvas.height, // アスペクト比は canvas のサイズそのまま
     near: 0.1, // ニアクリップ
     far: 100 // ファークリップ
   })
-  // 共通の変換行列を作っておく
-  pvMatrix = pMatrix.mulByMatrix4x4(vMatrix)
+
+  // テクスチャを生成
+  texture = await utils.getTexture(gl, './img/square-128x128.jpg')
 }
 
 /**
- * canvasに描画する関数
+ * 四角形をtranslateさせて描画する関数
+ *
+ * @param {number} tx
+ * @param {number} ty
+ * @param {number} tz
  */
-const draw = () => {
-  // canvasを初期化
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-
-  // カウンタをインクリメント
-  count++
-
-  // カウンタを元にラジアンを算出
-  const rad = ((count % 360) * Math.PI) / 180
-
-  // 回転するモデル
-  const mMatrix = Matrix4x4.identity().rotateAround(
-    new Float32Vector3(0.0, 1.0, 1.0).normalize(),
-    rad
-  )
+const drawSquare = (tx, ty, tz) => {
+  // モデル
+  const mMatrix = Matrix4x4.identity().translate(tx, ty, tz)
   const mvpMatrix = pvMatrix.mulByMatrix4x4(mMatrix)
   gl.uniformMatrix4fv(program.uMvpMatrix, false, mvpMatrix.values)
 
@@ -116,10 +144,56 @@ const draw = () => {
   gl.uniformMatrix4fv(program.uMInvMatrix, false, mInvMatrix.values)
 
   // 平行光源の向き
-  gl.uniform3fv(program.uLightDirection, [-0.5, 0.5, 0.5])
+  gl.uniform3fv(program.uLightDirection, lightDirection)
 
   // インデックスを用いた描画命令
   gl.drawElements(gl.TRIANGLES, index.length, gl.UNSIGNED_SHORT, 0)
+}
+
+/**
+ * canvasに描画する関数
+ */
+const draw = () => {
+  // canvasを初期化
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT)
+
+  // カウンタをインクリメント
+  count++
+
+  // カウンタを元にラジアンを算出
+  const rad = ((count % 360) * Math.PI) / 180
+
+  // ビュー座標変換行列
+  const vMatrix = Matrix4x4.lookAt(
+    new Float32Vector3(0.0, 0.0, 5.0), // 三次元空間を映し出すカメラを置く
+    new Float32Vector3(0.0, 0.0, 0.0), // 原点を注視点として見つめる
+    new Float32Vector3(0.0, 1.0, 0.0) // カメラの上方向は Y 軸の方向に指定
+  ).mulByMatrix4x4(rotationByMouse)
+
+  pvMatrix = pMatrix.mulByMatrix4x4(vMatrix)
+
+  // テクスチャ
+  gl.activeTexture(gl.TEXTURE0)
+  gl.bindTexture(gl.TEXTURE_2D, texture)
+  gl.uniform1i(program.uTexture, 0)
+
+  // ステンシルテストを有効にする
+  gl.enable(gl.STENCIL_TEST)
+
+  gl.stencilFunc(gl.ALWAYS, 1, ~0)
+  gl.stencilOp(gl.KEEP, gl.REPLACE, gl.REPLACE)
+  drawSquare(-0.25, 0.25, -0.5)
+
+  gl.stencilFunc(gl.ALWAYS, 0, ~0)
+  gl.stencilOp(gl.KEEP, gl.INCR, gl.INCR)
+  drawSquare(0.0, 0.0, 0.0)
+
+  gl.stencilFunc(gl.EQUAL, 2, ~0)
+  gl.stencilOp(gl.KEEP, gl.KEEP, gl.KEEP)
+  drawSquare(0.25, -0.25, 0.5)
+
+  // ステンシルテストを無効にする
+  gl.disable(gl.STENCIL_TEST)
 
   // コンテキストの再描画
   // 画面上にレンダリングされたモデルを描画するためには、コンテキストをリフレッシュする必要がある
@@ -143,21 +217,24 @@ const init = async () => {
   canvas.width = 500
   canvas.height = 300
 
-  gl = utils.getGLContext(canvas)
+  canvas.addEventListener('mousemove', calcQuaternionFromMousePosition, true)
+
+  gl = utils.getGLContext(canvas, { stencil: true })
 
   // canvasを初期化する色を設定する
-  gl.clearColor(0.0, 0.0, 0.0, 1.0)
+  gl.clearColor(0.0, 0.7, 0.7, 1.0)
   // canvasを初期化する際の深度を設定する
   gl.clearDepth(1.0)
+  //
+  gl.clearStencil(0)
 
-  // カリングと深度テストを有効にする
+  // 深度テストを有効にする
   gl.enable(gl.DEPTH_TEST)
   gl.depthFunc(gl.LEQUAL)
-  gl.enable(gl.CULL_FACE)
 
   // 適切な順序で関数を呼び出す
   await initProgram()
-  initBuffers()
+  await initBuffers()
   render()
 }
 
--- stencil-buffer-squares/index.vert
+++ stencil-buffer-squares/index.vert
@@ -3,6 +3,7 @@
 in vec3 a_position;
 in vec3 a_normal; // 法線
 in vec4 a_color;
+in vec2 a_textureCoord;
 
 uniform mat4 u_mvpMatrix;
 uniform mat4 u_mInvMatrix; // モデル座標変換行列の逆行列
@@ -10,6 +11,7 @@ uniform vec3 u_lightDirection; // 光の向き
 
 // フラグメントシェーダに渡す
 out vec4 v_Color;
+out vec2 v_TextureCoord;
 
 void main() {
   // モデルが回転などの座標変換を行なっていても、それと真逆の変換をライトベクトルに適用することで相殺する
@@ -18,5 +20,6 @@ void main() {
   // 0.1 <= dot <= 1.0 の範囲にclamp
   float diffuse = clamp(dot(a_normal, invLight), 0.1, 1.0);
   v_Color = a_color * vec4(vec3(diffuse), 1.0);
+  v_TextureCoord = a_textureCoord;
   gl_Position = u_mvpMatrix * vec4(a_position, 1.0);
 }
