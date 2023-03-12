9ec0da8d408c9aa1b71264ff4e5a3d56b21dd37d
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-03-12 10:01:02 +0900

    2本の線と球体を表す点

--- line-and-point-sphere/index.js
+++ line-and-point-sphere/index.js
@@ -1,10 +1,10 @@
-// @see https://wgld.org/d/webgl/w033.html
+// @see https://wgld.org/d/webgl/w036.html
 
 import { utils } from '../common/js/utils.js'
 import { Matrix4x4 } from '../common/js/dist/matrix.js'
 import { Float32Vector3 } from '../common/js/dist/vector.js'
 import { Quaternion } from '../common/js/dist/quaternion.js'
-import { torus } from './shape.js'
+import { sphere } from './shape.js'
 
 /** @type {HTMLCanvasElement | null} */
 let canvas = null
@@ -13,24 +13,28 @@ let gl = null
 /** @type {WebGLProgram | null} */
 let program = null
 
-/** @type {number[]} */
-let index = []
-
 /** @type {Matrix4x4} */
-let pvMatrix
+let pMatrix
 let rotationByMouse = Matrix4x4.identity()
+
+/** @type {{ position: WebGLBuffer; color: WebGLBuffer; length: number }} */
+let sphereBuffer = {
+  position: null,
+  color: null,
+  length: 0
+}
+/** @type {{ position: WebGLBuffer; color: WebGLBuffer; length: number }} */
+let lineBuffer = {
+  position: null,
+  color: null,
+  length: 0
+}
 
 let count = 0
 
-// 点光源の位置
-const lightPosition = [15.0, 10.0, 15.0]
-// 環境光の色
-const ambientColor = [0.1, 0.1, 0.1, 1.0]
-
-// カメラの座標
-const cameraPosition = [0.0, 0.0, 10.0]
-// カメラの上方向を表すベクトル
-const cameraUpDirection = [0.0, 1.0, 0.0]
+let pointSize = 1
+/** @type {'LINES' | 'LINE_STRIP' | 'LINE_LOOP'} */
+let linePrimitiveType = 'LINES'
 
 /**
  * イベントから取得したマウス座標をもとに回転軸ベクトルと回転角を割り出し、
@@ -55,6 +59,26 @@ const calcQuaternionFromMousePosition = (e) => {
 }
 
 /**
+ * GUIコントロール初期化関数
+ */
+const initControls = () => {
+  utils.configureControls({
+    'point size [px]': {
+      value: pointSize,
+      min: 1,
+      max: 10,
+      step: 0.1,
+      onChange: (v) => (pointSize = v)
+    },
+    'line primitive type': {
+      value: linePrimitiveType,
+      options: ['LINES', 'LINE_STRIP', 'LINE_LOOP'],
+      onChange: (v) => (linePrimitiveType = v)
+    }
+  })
+}
+
+/**
  * 適切な頂点シェーダーとフラグメントシェーダーでプログラムを作成する関数
  */
 const initProgram = async () => {
@@ -64,15 +88,10 @@ const initProgram = async () => {
   program = utils.getProgram(gl, vertexShader, fragmentShader)
 
   program.aVertexPosition = gl.getAttribLocation(program, 'a_position')
-  program.aNormal = gl.getAttribLocation(program, 'a_normal')
   program.aVertexColor = gl.getAttribLocation(program, 'a_color')
 
   program.uMvpMatrix = gl.getUniformLocation(program, 'u_mvpMatrix')
-  program.uMMatrix = gl.getUniformLocation(program, 'u_mMatrix')
-  program.uMInvMatrix = gl.getUniformLocation(program, 'u_mInvMatrix')
-  program.uLightPosition = gl.getUniformLocation(program, 'u_lightPosition')
-  program.uEyeDirection = gl.getUniformLocation(program, 'u_eyeDirection')
-  program.uAmbientColor = gl.getUniformLocation(program, 'u_ambientColor')
+  program.uPointSize = gl.getUniformLocation(program, 'u_pointSize')
 
   gl.useProgram(program)
 }
@@ -81,51 +100,78 @@ const initProgram = async () => {
  * バッファを準備する関数
  */
 const initBuffers = () => {
-  const torusData = torus(64, 64, 0.5, 1.5)
-  const { positions, colors, normals } = torusData
-  index = torusData.index
+  /* 球状に並んだ点 ------------------------------------ */
 
-  // 頂点位置情報VBO
-  utils.setAttribute(gl, {
-    vbo: utils.getVBO(gl, positions),
-    location: program.aVertexPosition,
-    stride: 3 // vec3型
-  })
+  const sphereData = sphere(16, 16, 2.0)
 
-  // 法線情報VBO
-  utils.setAttribute(gl, {
-    vbo: utils.getVBO(gl, normals),
-    location: program.aNormal,
-    stride: 3
-  })
+  sphereBuffer = {
+    position: utils.getVBO(gl, sphereData.positions),
+    color: utils.getVBO(gl, sphereData.colors),
+    length: sphereData.positions.length / 3
+  }
 
-  // 頂点色情報VBO
-  utils.setAttribute(gl, {
-    vbo: utils.getVBO(gl, colors),
-    location: program.aVertexColor,
-    stride: 4 // vec4型
-  })
+  /* 線 ------------------------------------------ */
 
-  // IBO
-  const ibo = utils.getIBO(gl, index)
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo)
+  const lineData = {
+    positions: [-1.0, -1.0, 0.0, 1.0, -1.0, 0.0, -1.0, 1.0, 0.0, 1.0, 1.0, 0.0],
+    colors: [1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0]
+  }
 
-  // ビュー座標変換行列
-  const vMatrix = Matrix4x4.lookAt(
-    new Float32Vector3(...cameraPosition), // 三次元空間を映し出すカメラを置く
-    new Float32Vector3(0.0, 0.0, 0.0), // 原点を注視点として見つめる
-    new Float32Vector3(...cameraUpDirection) // カメラの上方向
-  )
+  lineBuffer = {
+    position: utils.getVBO(gl, lineData.positions),
+    color: utils.getVBO(gl, lineData.colors),
+    length: lineData.positions.length / 3
+  }
+
+  /* -------------------------------------------- */
 
   // プロジェクション座標変換行列
-  const pMatrix = Matrix4x4.perspective({
+  pMatrix = Matrix4x4.perspective({
     fovYRadian: 45, // 視野角を 45 度
     aspectRatio: canvas.width / canvas.height, // アスペクト比は canvas のサイズそのまま
     near: 0.1, // ニアクリップ
     far: 100 // ファークリップ
   })
+}
 
-  pvMatrix = pMatrix.mulByMatrix4x4(vMatrix)
+/**
+ * 図形を描画する関数
+ *
+ * @param {Object} data
+ * @param {WebGLBuffer} data.position
+ * @param {WebGLBuffer} data.color
+ * @param {number} data.length
+ * @param {Matrix4x4} mMatrix
+ * @param {'LINES' | 'LINE_STRIP' | 'LINE_LOOP' | 'POINTS'} renderingType
+ */
+const drawShape = (data, mMatrix, renderingType) => {
+  // 頂点位置情報VBO
+  utils.setAttribute(gl, {
+    vbo: data.position,
+    location: program.aVertexPosition,
+    stride: 3 // vec3型
+  })
+
+  // 頂点色情報VBO
+  utils.setAttribute(gl, {
+    vbo: data.color,
+    location: program.aVertexColor,
+    stride: 4 // vec4型
+  })
+
+  // ビュー座標変換行列
+  const vMatrix = Matrix4x4.lookAt(
+    new Float32Vector3(0.0, 5.0, 10.0), // 三次元空間を映し出すカメラを置く
+    new Float32Vector3(0.0, 0.0, 0.0), // 原点を注視点として見つめる
+    new Float32Vector3(0.0, 1.0, 0.0) // カメラの上方向
+  ).mulByMatrix4x4(rotationByMouse)
+
+  // 最終的な座標変換行列
+  const mvpMatrix = pMatrix.mulByMatrix4x4(vMatrix).mulByMatrix4x4(mMatrix)
+  gl.uniformMatrix4fv(program.uMvpMatrix, false, mvpMatrix.values)
+
+  // 描画命令
+  gl.drawArrays(gl[renderingType], 0, data.length)
 }
 
 /**
@@ -137,33 +183,21 @@ const draw = () => {
 
   // カウンタをインクリメント
   count++
-
   // カウンタを元にラジアンを算出
-  const rad = ((count % 180) * Math.PI) / 90
+  const rad = ((count % 360) * Math.PI) / 180
 
-  // モデル座標変換行列
-  // マウスに応じて回転する
-  // マウスを動かさなくても自動的にy軸中心回転するようにする
-  const mMatrix = Matrix4x4.identity().mulByMatrix4x4(rotationByMouse).rotateY(rad)
-  gl.uniformMatrix4fv(program.uMMatrix, false, mMatrix.values)
+  // 点のサイズを送る
+  gl.uniform1f(program.uPointSize, pointSize)
 
-  // 最終的な座標変換行列
-  const mvpMatrix = pvMatrix.mulByMatrix4x4(mMatrix)
-  gl.uniformMatrix4fv(program.uMvpMatrix, false, mvpMatrix.values)
+  // 球を描画
+  const mMatrixSphere = Matrix4x4.identity().rotateY(rad)
+  drawShape(sphereBuffer, mMatrixSphere, 'POINTS')
 
-  // モデルの逆行列
-  const mInvMatrix = mMatrix.inverse()
-  gl.uniformMatrix4fv(program.uMInvMatrix, false, mInvMatrix.values)
-
-  // 点光源の位置
-  gl.uniform3fv(program.uLightPosition, lightPosition)
-  // 視線ベクトル
-  gl.uniform3fv(program.uEyeDirection, cameraPosition)
-  // 環境光の色
-  gl.uniform4fv(program.uAmbientColor, ambientColor)
-
-  // インデックスを用いた描画命令
-  gl.drawElements(gl.TRIANGLES, index.length, gl.UNSIGNED_SHORT, 0)
+  // 線を描画
+  const mMatrixLine = Matrix4x4.identity()
+    .rotateX(Math.PI / 2)
+    .scale(3.0, 3.0, 1.0)
+  drawShape(lineBuffer, mMatrixLine, linePrimitiveType)
 
   // コンテキストの再描画
   // 画面上にレンダリングされたモデルを描画するためには、コンテキストをリフレッシュする必要がある
@@ -184,7 +218,7 @@ const render = () => {
 const init = async () => {
   canvas = utils.getCanvas('webgl-canvas')
 
-  canvas.width = 300
+  canvas.width = 500
   canvas.height = 300
 
   canvas.addEventListener('mousemove', calcQuaternionFromMousePosition, true)
@@ -196,15 +230,20 @@ const init = async () => {
   // canvasを初期化する際の深度を設定する
   gl.clearDepth(1.0)
 
-  // カリングと深度テストを有効にする
+  // 深度テストを有効にする
   gl.enable(gl.DEPTH_TEST)
   gl.depthFunc(gl.LEQUAL)
-  gl.enable(gl.CULL_FACE)
+
+  // 点の最大ピクセル数をコンソールに表示
+  const [minPointSize, maxPointSize] = gl.getParameter(gl.ALIASED_POINT_SIZE_RANGE)
+  console.log('pointSizeRange:', minPointSize, 'to', maxPointSize)
 
   // 適切な順序で関数を呼び出す
   await initProgram()
   initBuffers()
   render()
+
+  initControls()
 }
 
 window.onload = init
